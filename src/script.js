47a29f3571356ace985128e320d5a388f399d5ec
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2022-06-18 20:46:38 +0300

    add basic 3d element

--- src/script.js
+++ src/script.js
@@ -1,174 +1,70 @@
 import './style.css'
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import * as dat from 'dat.gui'
-
-// Loading 
-
-const textureLoader = new THREE.TextureLoader()
-
-const normalTexture = textureLoader.load('/texture/NormalMap.png')
-
-// Debug
-const gui = new dat.GUI()
-
-// Canvas
-const canvas = document.querySelector('canvas.webgl')
-
-// Scene
-const scene = new THREE.Scene()
-
-// Objects
-const geometry = new THREE.SphereBufferGeometry( .5, 64, 64 );
-
-// Materials
-
-const material = new THREE.MeshStandardMaterial()
-material.metalness = 0.7
-material.roughness = 0.2
-material.normalMap = normalTexture;
-
-material.color = new THREE.Color(0x292929)
-
-// Mesh
-const sphere = new THREE.Mesh(geometry,material)
-scene.add(sphere)
-
-// Lights
-//Light 1
-const pointLight = new THREE.PointLight(0xffffff, 0.1)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
-scene.add(pointLight)
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 
-//Light 2
-const pointLight2 = new THREE.PointLight(0xff0000, 2)
-pointLight2.position.set(-1.86,1,-1.65)
-pointLight2.intensity = 10
+const scene = new THREE.Scene();
 
-scene.add(pointLight2)
-const light1 = gui.addFolder('Light 1')
-light1.add(pointLight2.position, 'y').min(-3).max(3).step(0.01)
-light1.add(pointLight2.position, 'x').min(-6).max(6).step(0.01)
-light1.add(pointLight2.position, 'z').min(-3).max(3).step(0.01)
-light1.add(pointLight2, 'intensity').min(0).max(10).step(0.01)
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000)
 
-// const pointLightHelper = new THREE.PointLightHelper(pointLight2,1)
-// scene.add(pointLightHelper)
-
-//Light 3
-const pointLight3 = new THREE.PointLight(0xe1ff, 2)
-pointLight3.position.set(2.13,-3,-1.98)
-pointLight3.intensity = 10
-
-scene.add(pointLight3)
-const light2 = gui.addFolder('Light 2')
-light2.add(pointLight3.position, 'y').min(-3).max(3).step(0.01)
-light2.add(pointLight3.position, 'x').min(-6).max(6).step(0.01)
-light2.add(pointLight3.position, 'z').min(-3).max(3).step(0.01)
-light2.add(pointLight3, 'intensity').min(0).max(10).step(0.01)
-
-
-const light2Color = {
-    color: 0xfff000
-}
-
-light2.addColor(light2Color, 'color')
-      .onChange(() => {
-        pointLight3.color.set(light2Color.color)
-      })
-
-// const pointLightHelper2 = new THREE.PointLightHelper(pointLight3,1)
-// scene.add(pointLightHelper2)
-/**
- * Sizes
- */
-const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
-}
-
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
-
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
-
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
-
-/**
- * Camera
- */
-// Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 0
-camera.position.y = 0
-camera.position.z = 2
-scene.add(camera)
-
-// Controls
-// const controls = new OrbitControls(camera, canvas)
-// controls.enableDamping = true
-
-/**
- * Renderer
- */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas,
-    alpha: true
+    canvas: document.querySelector('#bg')
 })
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-/**
- * Animate
- */
-document.addEventListener('mousemove', onDocumentMousemove)
+renderer.setPixelRatio( window.devicePixelRatio )
+renderer.setSize( window.innerWidth, window.innerHeight)
+camera.position.setZ(30)
 
-let mouseX = 0
-let mouseY = 0
 
-let targetX = 0
-let targetY = 0
 
-const windowHalfX = window.innerWidth / 2;
-const windowHalfY = window.innerHeight / 2;
+const geometry = new THREE.TorusGeometry(10,3,16,100)
+const material = new THREE.MeshStandardMaterial( {color: 0xFF6347} );
+const torus = new THREE.Mesh(geometry, material)
 
-function onDocumentMousemove(event) {
-    mouseX = (event.clientX - windowHalfX)
-    mouseY = (event.clientY - windowHalfY)
-}
-const clock = new THREE.Clock()
+scene.add(torus)
 
-const tick = () =>
-{
-      targetX = mouseX * .001
-      targetY = mouseY * .001
-    const elapsedTime = clock.getElapsedTime()
 
-    // Update objects
-    sphere.rotation.y = .5 * elapsedTime
-    
-    sphere.rotation.y += .5 * (targetX - sphere.rotation.y)
-    sphere.rotation.x += .05 * (targetY - sphere.rotation.z)
-    sphere.rotation.z += - .05 * (targetY - sphere.rotation.x)
-    // Update Orbital Controls
-    // controls.update()
+const pointLight = new THREE.PointLight(0xffffff)
+pointLight.position.set(2,2,2)
 
-    // Render
+const ambientLight = new THREE.AmbientLight(0xffffff)
+
+scene.add(pointLight, ambientLight )
+
+// const lightHelper = new THREE.PointLightHelper(pointLight)
+
+// scene.add(lightHelper)
+
+
+const control = new OrbitControls(camera, renderer.domElement)
+
+
+// const addStar = () => {
+//     const geometry = new THREE.SphereGeometry(0.25, 24 , 24)
+//     const material = new THREE.MeshStandardMaterial( {color: 0xffffff} );
+//     const star = new THREE.Mesh(geometry, material)
+
+
+//     const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread( 100 ))
+
+//     star.position.set(x,y,z)
+//     scene.add(star)
+// }
+
+
+// Array(200).fill().forEach(addStar)
+
+
+const spaceTexture = new THREE.TextureLoader().load('ss.jpg')
+scene.background = spaceTexture
+
+function animate() {
+    requestAnimationFrame(animate)
+
+    torus.rotation.x += 0.01
+    torus.rotation.y += 0.005
+    torus.rotation.z += 0.01
+     control.update()
     renderer.render(scene, camera)
-
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
 }
-
-tick()
+animate()
