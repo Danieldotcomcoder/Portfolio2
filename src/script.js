1900788227e67df4294225c8728c759f4651f6bd
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2022-06-19 12:19:21 +0300

    Add basic sphere

--- src/script.js
+++ src/script.js
@@ -43,47 +43,10 @@ pointLight.position.z = 4
 scene.add(pointLight)
 
 
-//Light 2
-const pointLight2 = new THREE.PointLight(0xff0000, 2)
-pointLight2.position.set(-1.86,1,-1.65)
-pointLight2.intensity = 10
 
-scene.add(pointLight2)
-const light1 = gui.addFolder('Light 1')
-light1.add(pointLight2.position, 'y').min(-3).max(3).step(0.01)
-light1.add(pointLight2.position, 'x').min(-6).max(6).step(0.01)
-light1.add(pointLight2.position, 'z').min(-3).max(3).step(0.01)
-light1.add(pointLight2, 'intensity').min(0).max(10).step(0.01)
-
-// const pointLightHelper = new THREE.PointLightHelper(pointLight2,1)
-// scene.add(pointLightHelper)
-
-//Light 3
-const pointLight3 = new THREE.PointLight(0xe1ff, 2)
-pointLight3.position.set(2.13,-3,-1.98)
-pointLight3.intensity = 10
-
-scene.add(pointLight3)
-const light2 = gui.addFolder('Light 2')
-light2.add(pointLight3.position, 'y').min(-3).max(3).step(0.01)
-light2.add(pointLight3.position, 'x').min(-6).max(6).step(0.01)
-light2.add(pointLight3.position, 'z').min(-3).max(3).step(0.01)
-light2.add(pointLight3, 'intensity').min(0).max(10).step(0.01)
-
-
-const light2Color = {
-    color: 0xfff000
-}
-
-light2.addColor(light2Color, 'color')
-      .onChange(() => {
-        pointLight3.color.set(light2Color.color)
-      })
-
-// const pointLightHelper2 = new THREE.PointLightHelper(pointLight3,1)
-// scene.add(pointLightHelper2)
 /**
  * Sizes
+ * Responsive
  */
 const sizes = {
     width: window.innerWidth,
@@ -115,9 +78,6 @@ camera.position.y = 0
 camera.position.z = 2
 scene.add(camera)
 
-// Controls
-// const controls = new OrbitControls(camera, canvas)
-// controls.enableDamping = true
 
 /**
  * Renderer
@@ -129,9 +89,15 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-/**
- * Animate
- */
+
+
+
+
+
+
+
+
+// onMouseMovement
 document.addEventListener('mousemove', onDocumentMousemove)
 
 let mouseX = 0
@@ -149,7 +115,14 @@ function onDocumentMousemove(event) {
 }
 const clock = new THREE.Clock()
 
-const tick = () =>
+
+
+
+
+
+// animate:
+
+const animate = () =>
 {
       targetX = mouseX * .001
       targetY = mouseY * .001
@@ -168,7 +141,7 @@ const tick = () =>
     renderer.render(scene, camera)
 
     // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
+    window.requestAnimationFrame(animate)
 }
 
-tick()
+animate()
