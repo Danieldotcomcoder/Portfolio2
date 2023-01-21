d4e798f20ccc19024f82feb02f7893383cb43a11
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2023-01-21 12:12:54 +0200

    fix geometry texture

--- script2.js
+++ script2.js
@@ -1,16 +1,13 @@
-import './style.css';
 import * as THREE from 'three';
-
 
 const scene = new THREE.Scene();
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-
 //Object 1
-const geometry = new THREE.SphereGeometry( 6,6,6 );
-const normalTexture = new THREE.TextureLoader().load('./static/texture/2kmars.jpg')
+const geometry = new THREE.SphereGeometry( 0.5,25,35 );
+const normalTexture = new THREE.TextureLoader().load('./main.jpg')
 const material = new THREE.MeshStandardMaterial({
     map: normalTexture,
 })
@@ -19,15 +16,18 @@ const material = new THREE.MeshStandardMaterial({
 const sphere = new THREE.Mesh(geometry,material)
 scene.add(sphere)
 
-
 // Light 1
-const pointLight = new THREE.PointLight( 0xffffff, 2)
-pointLight.position.set(-3,-1,-5)
-scene.add(pointLight)
+const light = new THREE.PointLight( 0xff0000, 0.2);
+light.position.set( 1, 5, 5 );
+scene.add( light );
 
+const lightHelper = new THREE.PointLightHelper(light)
+scene.add(lightHelper)
 
 // camera 
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-camera.position.z = 1;
+camera.position.z = 10;
+
+
 // render scene with camera
 renderer.render(scene, camera);
